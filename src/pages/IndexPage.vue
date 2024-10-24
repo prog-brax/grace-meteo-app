024734e1bb56a28b25ed7fe69cc5155ccc281764
Jérôme Mare <jerome.mare@gmail.com>
2024-10-24 19:56:50 +0200

    possibilié d'integration de latitude et longitude

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,4 +1,7 @@
 <template>
+  <q-input standout v-model="latitude" label="Latitude" />
+  <q-input standout v-model="longitude" label="Longitude" />
+  <q-btn color="amber" glossy label="Afficher" @click="searchMeteo" />
   <q-card class="my-card" v-for="(Code, index) in Codes" :key="index">
     <img
       :src="codesReference[Code].day.image"
@@ -28,15 +31,24 @@ const meteoD = ref("");
 const imageD = ref();
 const Codes = ref([]);
 const codesReference = ref(codes);
+const latitude = ref("40");
+const longitude = ref("2");
+
 onMounted(async () => {
+  searchMeteo();
+});
+
+async function searchMeteo() {
   const meteo = await axios.get(
-    "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,uv_index_clear_sky_max&current=weather_code"
+    "https://api.open-meteo.com/v1/forecast?latitude=" +
+      latitude.value +
+      "&longitude=" +
+      longitude.value +
+      "&daily=weather_code,temperature_2m_max,temperature_2m_min,sunrise,sunset,uv_index_max,uv_index_clear_sky_max&current=weather_code"
   );
   const etat = meteo.data.current.weather_code;
-  const T1 = meteo.data.current.temperature_2m_min;
-  const T2 = meteo.data.current.temperature_2m_max;
   Codes.value = meteo.data.daily.weather_code;
   imageD.value = codes[etat].day.image;
   meteoD.value = codes[etat].day.description;
-});
+}
 </script>
